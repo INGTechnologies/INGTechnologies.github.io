efa9d28d9600da8815d12a4763f49a958be2871c
n-c0d3r <dntechunity@gmail.com>
2022-06-13 14:01:44 +0700

    update core structure

---
+++ core/src/js/core.js
@@ -0,0 +1,13 @@
+
+import ui from './ui/ui.js'
+import docs from './docs/docs.js'
+
+
+
+export default {
+
+    ui : ui,
+
+    docs : docs
+
+}
---
+++ core/src/js/ui/ui.js
@@ -0,0 +1,16 @@
+
+import element_utils from './element_utils.js'
+import div from './div.js'
+import input from './input.js'
+
+
+
+export default {
+
+    element_utils : element_utils,
+
+    div : div,
+
+    input : input
+
+}
